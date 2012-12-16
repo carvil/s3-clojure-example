dc65a917e09fe3fe9be218cac3c3d3563520e0f1
Carlos Vilhena <carlosvilhena@gmail.com>
2012-12-16 18:32:16 +0000

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to s3-clojure-example
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/s3_clojure_example/core_test.clj
@@ -0,0 +1,7 @@
+(ns s3-clojure-example.core-test
+  (:use clojure.test
+        s3-clojure-example.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
