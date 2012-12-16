410b268cf7bfb248bcef2c15b13215b4af58efba
Carlos Vilhena <carlosvilhena@gmail.com>
2012-12-16 21:20:37 +0000

    load S3 config and setup credentials

--- src/s3_clojure_example/core.clj
+++ src/s3_clojure_example/core.clj
@@ -1,6 +1,17 @@
-(ns s3-clojure-example.core)
+(ns s3-clojure-example.core
+  (:require [clj-yaml.core :as yaml]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def s3-config-file
+  "Location of the config file"
+  "config/s3.yml")
+
+(defn raw-s3-credentials
+  "A raw string containing the S3 credentials read from the yaml file"
+  ([] (slurp s3-config-file))
+  ([filepath] (slurp filepath)))
+
+
+(defn s3-credentials
+  "A map of credentials read from the S3 yaml file"
+  ([] (yaml/parse-string (raw-s3-credentials)))
+  ([path] (yaml/parse-string (raw-s3-credentials path))))
--- test/s3_clojure_example/core_test.clj
+++ test/s3_clojure_example/core_test.clj
@@ -1,7 +1,15 @@
 (ns s3-clojure-example.core-test
   (:use clojure.test
-        s3-clojure-example.core))
+        [s3-clojure-example.core :exclude [s3-config-file]]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def s3-config-file-example "config/s3.example.yml")
+
+;; Tests for raw-s3-credentials-test
+(deftest raw-s3-credentials-test
+  (testing "Should read the example configurations"
+    (is (= (raw-s3-credentials s3-config-file-example) "access-key: my-key\nsecret-key: my-secret\nbuket: my-bucket\n"))))
+
+;; Tests for s3-credentials-test
+(deftest s3-credentials-test
+  (testing "Should return a map of configurations"
+    (is (= (s3-credentials s3-config-file-example) {:access-key "my-key", :secret-key "my-secret", :buket "my-bucket"}))))
